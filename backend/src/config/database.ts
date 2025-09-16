cc6de9a20f6c4bb89a27e4da1589dce8bf189a01
Lạc <metfc1.0@gmail.com>
2025-09-17 00:40:53 +0700

    add basic format

---
+++ backend/src/config/database.ts
@@ -0,0 +1,18 @@
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const supabaseURL = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_ANON_KEY!;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+
+export const supabase = createClient(supabaseURL, supabaseKey);
+export const supabaseAdmin = createClient(supabaseURL, supabaseServiceKey, {
+    auth: {
+        autoRefreshToken: false,
+        persistSession: false
+    }
+});
+
+export default supabase;
